0de53fdb066ee53db49a44f8e886472c23177aa5
Makary01 <makary.bortnowski@gmail.com>
2026-02-05 22:27:31 +0100

    working minimal sf.nvim config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,12 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
+    branch = 'master',
     config = function()
-        require('nvim-treesitter').install{
-            'lua',
-            'javascript',
+        --require('nvim-treesitter').install{ 'lua', 'javascript', 'sflog', 'apex', 'soql', 'sosl', 'markdown', }
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+            ensure_installed = { 'lua', 'javascript', 'sflog', 'apex', 'soql', 'sosl', 'markdown', },
         }
     end
 }
